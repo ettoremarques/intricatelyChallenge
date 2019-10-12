373e10de0d0b4ce1e8c577e6ce8e349560e4bad5
Ettore <emcimino1@hotmail.com>
2019-10-12 16:59:40 -0300

    Started doing company list component

---
+++ src/components/CompanyList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="companyList">
+    <div class="adress">
+        <a> {{ pageAdress }} / {{ currentAdress }} </a>
+    </div>
+    <div class="list">
+      <div 
+        v-for="(item, index) in this.CompanyListDataArray" 
+        :key="index">
+
+        <a>{{ item }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    CompanyListData: Array,
+    Adress: Object
+  },
+  data() {
+    return {
+      CompanyListDataArray: this.CompanyListData,
+      pageAdress: this.Adress.page,
+      currentAdress: this.Adress.current
+    };
+  }
+};
+</script>
+
+<style lang="css">
+    @import url("../styles/CompanyList.css");
+</style>
