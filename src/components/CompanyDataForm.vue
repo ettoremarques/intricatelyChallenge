c4e2573d02a009652452e67746f8896923d15b4a
Ettore Marques <ettore.marques@madeinweb.com.br>
2019-10-16 18:38:22 -0300

    making company data form without validations

---
+++ src/components/CompanyDataForm.vue
@@ -0,0 +1,33 @@
+<<template>
+    <div class="companyDataForm">
+        <div class="form-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sequi praesentium eaque doloremque qui voluptatem hic rerum voluptates nostrum ex ad tempore odit</p>
+        </div>
+        <div class="data-form">
+            <div class="company">
+                <label>Company Name</label>
+                <input class="form-input" placeholder="e.g Your Company Name">
+            </div>
+            <div class="company">
+                <label>Company Spend</label>
+                <input class="form-input" placeholder="e.g $150,000">
+            </div>
+            <div class="company">
+                <label>Company Spend Ability</label>
+                <input class="form-input" placeholder="e.g $150,000 - $330,000">
+            </div>
+            <div class="company">
+                <label>Notes</label>
+                <textarea class="form-textarea" placeholder="e.g Good Tech Company"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+   @import url('../styles/CompanyDataForm.css'); 
+</style>
